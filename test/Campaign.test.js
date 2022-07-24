d1f06c3d68cdb44cf6ac585fc9b50f579b41a4b0
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2022-07-24 10:28:33 +0530

    Added more test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -80,4 +80,106 @@ describe('Campaign', () => {
             assert(error);
         }
     });
+    it('checking if the contract allows the manager to create a payment request', async() => {
+        await campaign.methods
+            .createRequest(
+                'Raising money for buying motor components',
+                '500',
+                accounts[1],
+            )
+            .send({
+                from: accounts[0],
+                gas: '6721975',
+            });
+
+        let requestCount = await campaign.methods.getRequestsCount().call();
+        assert(requestCount > 0);
+    });
+    it('allow manager to apporve a request', async() => {
+        await campaign.methods.addContributers().send({
+            value: web3.utils.toWei('10', 'ether'),
+            from: accounts[1],
+        });
+        await campaign.methods
+            .createRequest(
+                'Raising money for buying motor components',
+                web3.utils.toWei('10', 'ether'),
+                accounts[2],
+            )
+            .send({
+                from: accounts[0],
+                gas: '6721975',
+            });
+        await campaign.methods.approveRequest(0).send({
+            from: accounts[1],
+            gas: '6721975',
+        });
+        await campaign.methods.finalizeRequest(0).send({
+            from: accounts[0],
+            gas: '6721975',
+        });
+
+        let balance = await web3.eth.getBalance(accounts[2]);
+        balance = web3.utils.fromWei(balance, 'ether');
+        balance = parseFloat(balance);
+
+        assert(balance > 104);
+    });
+    it('only allow contributers to approve a request', async() => {
+        try {
+            await campaign.methods.approveRequest(0).send({
+                from: accounts[1],
+                gas: '6721975',
+            });
+            assert(false);
+        } catch (error) {
+            assert(error);
+        }
+    });
+    it('only allow manager to finalize a request', async() => {
+        try {
+            await campaign.methods
+                .createRequest(
+                    'Raising money for buying motor components',
+                    web3.utils.toWei('10', 'ether'),
+                    accounts[2],
+                )
+                .send({
+                    from: accounts[0],
+                    gas: '6721975',
+                });
+            await campaign.methods.finalizeRequest(0).send({
+                from: accounts[1],
+                gas: '6721975',
+            });
+            assert(false);
+        } catch (error) {
+            assert(error);
+        }
+    });
+    it('manager can only process request if it has enough approvers', async() => {
+        try {
+            await campaign.methods.addContributers().send({
+                value: web3.utils.toWei('10', 'ether'),
+                from: accounts[1],
+            });
+            await campaign.methods
+                .createRequest(
+                    'Raising money for buying motor components',
+                    web3.utils.toWei('10', 'ether'),
+                    accounts[2],
+                )
+                .send({
+                    from: accounts[0],
+                    gas: '6721975',
+                });
+            await campaign.methods.finalizeRequest(0).send({
+                from: accounts[0],
+                gas: '6721975',
+            });
+            assert(false);
+        } catch (error) {
+            assert(error);
+        }
+    });
 });
