1214f00a0ca22d0a8820f9e05e0ea87620aca8f6
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2022-07-25 13:53:39 +0530

    Fixed bugs in deploy scripts and created eviroment variables

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,25 +1,27 @@
 // this script deploy the complied contract from build to etherum network
-
-const HDWalledtProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
+const HDWalledtProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
-const provider = new HDWalledtProvider(
-    //your mimic code
-    //a rinkbey node on infura
-    'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q',
-);
+console.log(process.env.METAMASK_ACCOUNT_MEMONIC);
+const provider = new HDWalledtProvider({
+    mnemonic: {
+        phrase: process.env.METAMASK_ACCOUNT_MEMONIC,
+    },
+    providerOrUrl: process.env.RINKBEY_INFURA_NODE,
+});
 const web3 = new Web3(provider);
 
 const deploy = async() => {
     const accounts = await web3.eth.getAccounts();
 
     //now deploying the complied contract on the infura rinkbey node.
-    const result = await new web3.eth.Contract(
-            JSON.parse(compiledFactory.interface),
-        )
-        .deploy({ data: compiledFactory.bytecode })
-        .send({ gas: '1000000', from: accounts[0] });
+    const result = await new web3.eth.Contract(compiledFactory.abi)
+        .deploy({ data: compiledFactory.evm.bytecode.object })
+        .send({ gas: '6721975', from: accounts[0] });
 
     console.log('Contracts deployed to', result.options.address);
 };
+
+deploy();
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,13 +1,15 @@
+import dotenv from 'dotenv';
 import Web3 from 'web3';
 let web3;
 
+dotenv.require();
 if (typeof window !== undefined && typeof window.web3 !== undefined) {
     // this means we are in a brwoser with metamask installed
     web3 = new Web3(window.web3.currentProvider);
 } else {
     // this means we on the server or the user doesn't running metamask
     const provider = new Web3.providers.HttpProvider(
-        'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q',
+        process.env.RINKBEY_INFURA_NODE,
     );
     //we got a rinbey node hosted on infura
     web3 = new Web3(provider);
